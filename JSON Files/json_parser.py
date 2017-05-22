dbd19ce8e1bab25ac81492a0d670e1e5b8540626
William Cox <apple1@William-Cox.local>
2017-05-21 19:03:53 -0500

    add noun corpus

--- JSON Files/json_parser.py
+++ JSON Files/json_parser.py
@@ -2,6 +2,8 @@ import json
 
 
 file_list = list()
+pos = 'n'
+output_file = 'noun_corpus.txt'
 
 for ii in range(1,34):
     file_list.append("json_"+str(ii)+".json")
@@ -20,7 +22,7 @@ def data_retrieval(fid):
             #temp_2 = data["treebank"]["body"]["sentence"][ii]["word"][jj]
             try:
                 temp_2 = data["treebank"]["body"]["sentence"][ii]["word"][jj]['_postag']
-                if temp_2.encode('utf-8')[0] == 'v':
+                if temp_2.encode('utf-8')[0] == pos:
                     lemma_list.append(data["treebank"]["body"]["sentence"][ii]["word"][jj]['_lemma'])
             except:
                 True
@@ -30,7 +32,7 @@ corpus = []
 for kk in range(len(file_list)):
     corpus = corpus + data_retrieval(file_list[kk])
     
-cfid = open('verb_corpus.txt','w')
+cfid = open(output_file,'w')
 for verb in corpus:
     cfid.write("%s\n" % verb.encode('utf-8'))
         
